2aa31800ecb538ca0124b269729561f218bd0b95
Oleg Golubev <traicer95@gmail.com>
2024-06-29 16:13:21 +0300

    refactoring sem2.hw.BubbleSort

--- src/main/java/org/example/sem2/hw/BubbleSort.java
+++ src/main/java/org/example/sem2/hw/BubbleSort.java
@@ -3,52 +3,61 @@ package org.example.sem2.hw;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 
 public class BubbleSort {
-    private static File log = new File("log.txt");
+    private static File log;
     private static FileWriter fileWriter;
 
     public static void sort(int[] mas) {
-        String res = "";
-        String last = "";
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 0; j < mas.length - 1 - i; j++) {
-                if (mas[j] > mas[j + 1]) {
-                    int temp = mas[j];
-                    mas[j] = mas[j + 1];
-                    mas[j + 1] = temp;
-                }
+        try {
+            log = new File("log.txt");
+            log.createNewFile();
+            fileWriter = new FileWriter(log);
+            bubbleSort(mas);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
 
-            LocalDateTime dateTime = LocalDateTime.now();
-            int year = dateTime.getYear();
-            int month = dateTime.getMonthValue();
-            int day = dateTime.getDayOfMonth();
-            int hour = dateTime.getHour();
-            int min = dateTime.getMinute();
-            String date = String.format("%d-0%d-%d %d:%d ", year, month, day, hour, min);
-            String state = "[";
-            for (int j = 0; j < mas.length - 1; j++) {
-                state += String.valueOf(mas[j] + ", ");
-            }
-            state += String.valueOf(mas[mas.length - 1]) + "]\n";
-            res += date + state;
-            if (last.equals(state)){
-                break;
-            }else{
-                last = state;
-            }
         }
-        try (FileWriter fw = new FileWriter(log)) {
-            fw.write(res);
+    }
+    private static int[] bubbleSort(int[] mas){
+        boolean isSorted = false;
+        int buffer;
+        while (!isSorted){
+            isSorted = true;
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] > mas[i + 1]){
+                    isSorted = false;
+                    buffer = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = buffer;
+                }
+            }
+            logSteps(Arrays.toString(mas));
+        }
+        return mas;
+    }
+
+    public static void logSteps(String strArray){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        String dateTime = dateFormat.format(new Date());
+        try {
+            fileWriter.write(dateTime + " " + strArray + "\n");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
     public static void main(String[] args) {
         int[] arr = new int[]{9, 3, 4, 8, 2, 5, 7, 1, 6, 10};
         sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
