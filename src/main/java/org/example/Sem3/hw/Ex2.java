5c0bf9599723ab26940a72281cea5121e346a4f8
Oleg Golubev <traicer95@gmail.com>
2024-07-02 12:14:07 +0300

    compltet Ex2

--- src/main/java/org/example/Sem3/hw/Ex2.java
+++ src/main/java/org/example/Sem3/hw/Ex2.java
@@ -1,7 +1,27 @@
 package org.example.Sem3.hw;
 
-public class Ex2 {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Ex2 {
+    /**
+     * Дан произвольный массив целых чисел.
+     * Создайте список ArrayList, заполненный данными из этого массива.
+     * Необходимо удалить из списка четные числа и вернуть результирующий.
+     */
+    public static ArrayList<Integer> removeEvenNumbers(Integer[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer i : arr) {
+            if (i%2!=0){
+                list.add(i);
+            }
+        }
+
+        return list;
+    }
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> onlyOddNumbers = removeEvenNumbers(arr);
+        System.out.println(onlyOddNumbers);
     }
 }
